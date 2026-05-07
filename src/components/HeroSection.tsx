acb78734da2e9b9b91ff89babe4207ca2a1cb792
ashish200729 <darkaneesport@gmail.com>
2026-05-07 16:36:33 +0530

    updated resume

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -125,7 +125,7 @@ export default function HeroSection({ scrollToSection }: { scrollToSection: (id:
             </div>
 
             <div className="flex flex-wrap items-center gap-3 mt-6">
-              <a href="/ashish_resume_new4.pdf" target="_blank" rel="noopener noreferrer" className="px-4 py-2.5 bg-white dark:bg-[#0f0f0f] hover:bg-gray-50 dark:hover:bg-[#1a1a1a] border border-gray-200 dark:border-gray-800 hover:border-gray-300 dark:hover:border-gray-700 rounded-lg transition-colors flex items-center gap-2 text-gray-900 dark:text-white text-sm font-medium shadow-sm">
+              <a href="/Ashish_Prajapati_Resume.pdf" target="_blank" rel="noopener noreferrer" className="px-4 py-2.5 bg-white dark:bg-[#0f0f0f] hover:bg-gray-50 dark:hover:bg-[#1a1a1a] border border-gray-200 dark:border-gray-800 hover:border-gray-300 dark:hover:border-gray-700 rounded-lg transition-colors flex items-center gap-2 text-gray-900 dark:text-white text-sm font-medium shadow-sm">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 256 256" className="w-4 h-4">
                   <path d="M210.78,39.25l-130.25-23A16,16,0,0,0,62,29.23l-29.75,169a16,16,0,0,0,13,18.53l130.25,23h0a16,16,0,0,0,18.54-13l29.75-169A16,16,0,0,0,210.78,39.25ZM178.26,224h0L48,201,77.75,32,208,55ZM89.34,58.42a8,8,0,0,1,9.27-6.48l83,14.65a8,8,0,0,1-1.39,15.88,8.36,8.36,0,0,1-1.4-.12l-83-14.66A8,8,0,0,1,89.34,58.42ZM83.8,89.94a8,8,0,0,1,9.27-6.49l83,14.66A8,8,0,0,1,174.67,114a7.55,7.55,0,0,1-1.41-.13l-83-14.65A8,8,0,0,1,83.8,89.94Zm-5.55,31.51A8,8,0,0,1,87.52,115L129,122.29a8,8,0,0,1-1.38,15.88,8.27,8.27,0,0,1-1.4-.12l-41.5-7.33A8,8,0,0,1,78.25,121.45Z"></path>
                 </svg>
